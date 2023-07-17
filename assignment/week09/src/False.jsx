42cf2f68d64dfc2fe5007c7dedaf06a5796bb1f7
solux <lee.yeji0327@gmail.com>
2023-07-17 23:41:44 +0900

    과제

---
+++ assignment/week09/src/False.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const False = () => {
+    const state = "False";
+    const name = "예지";
+    const lion = "아기사자";
+    return (
+        <div>
+            <h4>{state} : 저는 {lion} {name}🦁 가 아닙니다.</h4>
+        </div>
+    );
+};
+
+export default False;
