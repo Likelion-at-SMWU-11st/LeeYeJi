5f288f6b4c9d8717d46ec51da06a69d0cbfc10fa
solux <lee.yeji0327@gmail.com>
2023-07-17 22:08:10 +0900

    실습

---
+++ session/front/week03/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hello from "./Hello";
+
+function App() {
+  return (
+    <>
+      <h3>멋사 프론트 3주차 세미나</h3>
+      <p>화이팅 ㅎㅎㅎ</p>
+      <Hello />
+    </>
+  );
+}
+
+export default App;
