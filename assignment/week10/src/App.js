d849560a7fcdd1730f37bd28993f30dd1c694541
solux <lee.yeji0327@gmail.com>
2023-07-22 19:02:24 +0900

    프론트 4주차 과제

--- assignment/week10/src/App.js
+++ assignment/week10/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Title from './Title';
+import Counter from './Counter';
+import MusicList from './MusicList';
 
 function App() {
   return (
     <>
-      
+      <Title title="Fav Music 🎧"/>
+      <Counter name="LeeYeJi"/>
+      <MusicList />
     </>
   );
 }
---
+++ assignment/week10/src/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../src/App.css'
+
+function Title ({ title, name }) {
+    return (
+        <div class="body">
+            <h1>{title}</h1>
+        </div>
+    );
+};
+
+export default Title;
