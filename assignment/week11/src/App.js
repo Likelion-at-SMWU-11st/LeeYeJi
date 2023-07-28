b0b2b088cb63833358742ba665cf97060a4d3b2f
solux <lee.yeji0327@gmail.com>
2023-07-28 19:56:00 +0900

    프론트 5주차 과제

---
+++ assignment/week11/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import TopBar from './TopBar';
+import Main from './Main';
+import Post from './Post';
+
+function App() {
+  return (
+    <>
+      <TopBar />
+      <Main />
+      <Post title="지금 핫한 글 ☀️" info="서핑 트렌드 보기"/>
+      <Post title="Beginner 🏄🏻‍♀️" info="서핑 입문하기"/>
+      <Post title="Master 🏄🏻" info="서핑 고수들의 정보 방출"/>
+      <Post title="Together 🏄🏻‍♂️" info="함께 서핑하기"/>
+    </>
+  );
+}
+
+export default App;
