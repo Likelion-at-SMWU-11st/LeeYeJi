47bca3ed2c3cb130fbb32878cc523affcfc7d133
solux <lee.yeji0327@gmail.com>
2023-08-03 22:27:37 +0900

    add Menubar ui

--- assignment/week12/src/pages/Menubar.js
+++ assignment/week12/src/pages/Menubar.js
@@ -3,19 +3,36 @@ import styled from 'styled-components';
 import { Link, Outlet } from 'react-router-dom';
 
 const Bar = styled.div`
+    width: 100%;
+    height: 100px;
+`
+const Txt = styled.h2`
+    margin-left: 20px;
+`
+const NaviContainer = styled.div`
+`
+const Navi = styled.div`
+    color: black;
+    text-decoration: none;
+    //background-color: 
+    font-size: 20px;
+    font-weight: 600;
+    padding: 10px 20px 10px 20px;
+    border: 1.5px solid black;
+    border-radius: 50px;
+    width: fit-content;
+    margin: 5px;
 `
 
 const Menubar = () => {
     return (
         <Bar>
-            <ul>
-                <li>
-                    <Link to="/surf">SurfPage</Link>
-                </li>
-                <li>
-                    <Link to="/music">MusicPage</Link>
-                </li>
-            </ul>
+            <Txt>To go ..</Txt>
+            <NaviContainer>
+                    <Link to="/surf" style={{ textDecoration: "none" }}><Navi>🏄🏻‍♂️ SurfPage</Navi></Link>
+
+                    <Link to="/music" style={{ textDecoration: "none" }}><Navi>🎧 MusicPage</Navi></Link>
+                </NaviContainer>
             <Outlet />
         </Bar>
     );
