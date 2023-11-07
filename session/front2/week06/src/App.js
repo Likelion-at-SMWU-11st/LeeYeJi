dfa25dec0104c74aead2dba84555af3f5e5ea501
solux <lee.yeji0327@gmail.com>
2023-11-07 21:52:55 +0900

    redux session

---
+++ session/front2/week06/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SampleContainer from './components/SampleContainer';
+import CounterContainer from './containers/CounterContainer';
+
+function App() {
+  return (
+    <div>
+      <SampleContainer/>
+    </div>
+  );
+}
+
+export default App;
